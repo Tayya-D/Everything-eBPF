51c00e49053f35179d37a6ee3aadaef6e802ce63
Tayya-D <tayybahdar@gmail.com>
2025-06-29 17:19:16 +0100

    hello-world but using map functionality, naming convention update + notes

---
+++ eBPF-files/002-ebpf-hello-world-map.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# This script demonstrates a simple eBPF program that prints "Hello, World!" to the kernel log.
+# It uses the BCC (BPF Compiler Collection) library to load and run the eBPF program.
+# Make sure you have BCC installed on your system to run this script.
+from bcc import BPF
+from time import sleep
+
+# Define the eBPF program as a string
+program = """ 
+
+BPF_HASH(clones ); // Define a hash map to store clone syscall data associated with UIDs
+
+int hello_world(void *ctx) {
+    u64 uid;
+    u64 counter = 0;
+    u64 *ptr;
+
+    uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    ptr - clones.lookup(uid);
+    if (ptr) {
+        counter = *ptr;
+    }
+    // Increment the counter for the current UID
+    counter++;
+    clones.update(uid, &counter);
+    
+    // Print the UID of the current process
+    bpf_trace_printk("Hello, World! UID: %d\\n", uid);
+    return 0;
+}
+"""
+
+# Load the eBPF program
+b = BPF(text=program)
+
+# Get the syscall number for 'clone'
+# 'clone' is a common syscall used for creating processes, and we will attach our eBPF program to it.
+clone = b.get_syscall_fnname("clone")
+
+# Attach the eBPF program to the 'clone' syscall
+b.attach_kprobe(event=clone, fn_name="hello_world")
+
+# Print the output
+print("Tracing... Hit Ctrl-C to end.")
+try:
+    sleep(999999)
+except KeyboardInterrupt:
+    pass
+# Print the trace output
+print("Output:")
+b.trace_print()
--- eBPF-files/notes.md
+++ eBPF-files/notes.md
@@ -2,3 +2,5 @@
 
 The 'bpf_trace_printk' function is ok to use for simple programs like the hello world program but not for production level programs.
 This is because the 'bpf_trace_printk' function prints the output to a single pipe. This is the same for the '.trace_print()' function where it reads from a single pipe -> this won't really scale well when you start wanting to run multiple eBPF programs
+
+-> Instead you can ustilise maps and key value pairs
