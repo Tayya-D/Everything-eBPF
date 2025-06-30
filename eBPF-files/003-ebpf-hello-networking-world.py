ccff5f962c49bbec48a6a00c9c8eb24d457e6dd2
Tayya-D <tayybahdar@gmail.com>
2025-06-30 12:09:43 +0100

    Define eBPF program as a string

--- eBPF-files/003-ebpf-hello-networking-world.py
+++ eBPF-files/003-ebpf-hello-networking-world.py
@@ -4,7 +4,20 @@ import os
 from time import sleep
 from pyroute2 import IPRoute
 
+# Define the eBPF program as a string
+ebpf_hello_networking_world_code = """ 
 
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/pkt_cls.h>
+
+int tcpconnect(void *ctx) {
+    bpf_trace_printk("[tcpconnect] TCP connection established\\n");
+    return 0;
+
+}
+
+"""
 
 interface = "eth0"  # Change this to your network interface
 
