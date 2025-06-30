4a13ea70de1e154c1af74b3741abca7a59ff2236
Tayya-D <tayybahdar@gmail.com>
2025-06-30 13:33:18 +0100

    socket filtering program to send copy of TCP packets to userspace

--- eBPF-files/003-ebpf-hello-networking-world.py
+++ eBPF-files/003-ebpf-hello-networking-world.py
@@ -11,12 +11,41 @@ ebpf_hello_networking_world_c_code = """
 #include <linux/ip.h>
 #include <linux/pkt_cls.h>
 
+/// This eBPF program traces TCP connections and prints a message when an entry is created.
+
 int tcpconnect(void *ctx) {
     bpf_trace_printk("[tcpconnect] TCP connection established\\n");
     return 0;
 
 }
 
+/// The following function filters socket events based on the network interface.
+
+int socket_filter(struct __sk_buff *skb) {
+    unsigned char *cursor = 0;
+
+    struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
+
+    // Look for IP packets
+    if (ethernet -> type != 0x0800) {
+        return 0;  // Not an IP packet
+    }
+    
+    struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
+    
+    // Look at the IP header to determine the type of packet
+    
+    if (ip -> nextp == 0x01) {  // Check if the next protocol is ICMP (otherwise known as ping)
+        bpf_trace_printk("[socket_filter] ICMP packet detected for %x\\n", ip -> dst);
+    } else if (ip -> nextp == 0x11) {  // Check if the next protocol is UDP
+        bpf_trace_printk("[socket_filter] UDP packet detected for %x\\n", ip -> dst);
+    }  else if (ip -> nextp == 0x06) {  // Check if the next protocol is TCP
+        bpf_trace_printk("[socket_filter] TCP packet detected for %x\\n", ip -> dst);
+        // Send a copy of this TCP packet to userspace 
+        return -1;
+    }
+    return 0;  // Allow all other packets to pass through as normal - not sending any to userspace
+}
 
 
 """
